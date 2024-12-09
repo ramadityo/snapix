df54df342cf64b8de80267ddc691b44ca8e0d20a
Vynn0 <halvinonata@gmail.com>
2024-12-09 11:30:44 +0700

    fix 2

--- database/migrations/2024_11_21_144104_create_images_table.php
+++ database/migrations/2024_11_21_144104_create_images_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('log_images');
+        Schema::dropIfExists('images');
     }
 };
